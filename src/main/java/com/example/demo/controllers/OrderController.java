52e5de6ebd2362159b1f30a94c947947dd624798
alhamami <jalalalhamami@gmail.com>
2025-01-01 01:39:22 +0300

    Implemented OrderController for order

--- src/main/java/com/example/demo/controllers/OrderController.java
+++ src/main/java/com/example/demo/controllers/OrderController.java
@@ -2,6 +2,8 @@ package com.example.demo.controllers;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.persistence.Cart;
 import com.example.demo.model.persistence.User;
 import com.example.demo.model.persistence.UserOrder;
-import com.example.demo.model.persistence.repositories.CartRepository;
 import com.example.demo.model.persistence.repositories.OrderRepository;
 import com.example.demo.model.persistence.repositories.UserRepository;
 
@@ -22,21 +22,27 @@ import com.example.demo.model.persistence.repositories.UserRepository;
 public class OrderController {
 	
 	
-	@Autowired
-	private UserRepository userRepository;
-	
-	@Autowired
-	private OrderRepository orderRepository;
-	
-	
+	private final UserRepository userRepository;
+	private final OrderRepository orderRepository;
+
+
+	public static final Logger logger = LogManager.getLogger(UserController.class);
+
+	public OrderController(UserRepository userRepository, OrderRepository orderRepository) {
+		this.userRepository = userRepository;
+		this.orderRepository = orderRepository;
+	}
+
 	@PostMapping("/submit/{username}")
 	public ResponseEntity<UserOrder> submit(@PathVariable String username) {
 		User user = userRepository.findByUsername(username);
 		if(user == null) {
+			logger.error("Fail to create order, username ["+username+"] not found");
 			return ResponseEntity.notFound().build();
 		}
 		UserOrder order = UserOrder.createFromCart(user.getCart());
 		orderRepository.save(order);
+		logger.info("Order created successfully "+order.toString());
 		return ResponseEntity.ok(order);
 	}
 	
@@ -44,8 +50,18 @@ public class OrderController {
 	public ResponseEntity<List<UserOrder>> getOrdersForUser(@PathVariable String username) {
 		User user = userRepository.findByUsername(username);
 		if(user == null) {
+			logger.error("Fail to get history, username ["+username+"] not found");
 			return ResponseEntity.notFound().build();
 		}
-		return ResponseEntity.ok(orderRepository.findByUser(user));
+
+		List<UserOrder> orders = orderRepository.findByUser(user);
+
+		if (orders.isEmpty()) {
+			logger.info("No order found for username ["+username+"]");
+		} else {
+			logger.info("Orders found Successfully for username ["+username+"]");
+		}
+
+		return ResponseEntity.ok(orders);
 	}
 }
